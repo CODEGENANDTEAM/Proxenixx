e079fae4f108c85e73ba38e7935652de878d4c6c
CODEGENANDTEAM <team.codegen@gmail.com>
2025-05-04 11:12:24 +0530

    Updated cta.tsx
    FORM ADD

--- components/cta.tsx
+++ components/cta.tsx
@@ -36,7 +36,7 @@ export default function Cta() {
             <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
               <a
                 className="btn group mb-4 w-full bg-linear-to-t from-blue-600 to-blue-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-sm hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                href="#0"
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdsoLL7Ycr6FOIigXny-u3d7uHsLyndsaJwNEiK7mG8NMfHlA/viewform?usp=dialog"
               >
                 <span className="relative inline-flex items-center">
                   Start Free Trial{" "}
